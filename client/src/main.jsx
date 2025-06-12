db1fb901600e5b76fc8bffc472bca27366d3b395
Tanu Tripathi <139795364+tanutripathi1810@users.noreply.github.com>
2025-06-13 04:29:39 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import AppContextProvider from './context/AppCOntext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 import App from './App.jsx'
 import './index.css'
 

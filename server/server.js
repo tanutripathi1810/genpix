31b382b164b8a4b0a9fb15596eae1050a4c58130
Tanu Tripathi <tripathitanu02@gmail.com>
2025-06-13 22:05:15 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ const PORT = process.env.PORT || 4000;
 const app = express()
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000', 
+    'https://genpix-6ys4.vercel.app',
+    'https://*.vercel.app'
+  ],
+  credentials: true
+}));
 await connectDB();
 
 app.use('/api/user',userRouter);
